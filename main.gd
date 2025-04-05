0e2c705f8944370f4d43fc1c1d60c56354c896f0
Ben Eng <whysguy52@gmail.com>
2025-04-05 00:07:44 -0500

    main script 2D array gen

--- main.gd
+++ main.gd
@@ -1,1 +1,24 @@
 extends Node3D
+
+var roomGrid = []
+var gridWidth = 10
+var gridHeight = 10
+var level = 1
+
+var row = 3
+var column = 3
+var numOfRooms:int
+
+func _ready() -> void:
+  #create the 2D array
+  for i in gridWidth:
+    roomGrid.append([])
+    for j in gridHeight:
+      roomGrid[i].append(Node)
+  numOfRooms = (randi() % 2) + 5 + (level * 2.6)
+  print(numOfRooms)
+
+
+func createGrid():
+
+  pass
