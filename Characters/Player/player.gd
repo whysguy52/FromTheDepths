bdc2c576736c43c17318002fde9511171bedd541
Ben Eng <whysguy52@gmail.com>
2025-04-06 14:27:06 -0500

    blocking function but not perfect

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody3D
 
-var sword
-var heldWeapon
+
 var mouseSens = 0.0005
 var speed = 250
 
@@ -11,19 +10,32 @@ var animPlayer:AnimationPlayer
 var handL
 var handR
 
-func _ready() -> void:
-  #load sword
-  sword = load("res://Assets/Items/Weapons/Sword/sword.tscn")
-  heldWeapon = sword.instantiate()
-  $Player2/HandR.add_child(heldWeapon)
+#scene holders
+var sword
+var heldWeapon
+var heldSheild
+var sheild
 
+func _ready() -> void:
   #references
   cameraNod = $CameraNod
   handL = $Player2/HandL
   handR = $Player2/HandR
+  animPlayer = $Player2/AnimationPlayer
+
+  #load sword
+  sword = load("res://Assets/Items/Weapons/Sword/sword.tscn")
+  heldWeapon = sword.instantiate()
+  handR.add_child(heldWeapon)
+
+  #load sheild
+  sheild = load("res://Assets/Items/Weapons/Sheild/Sheild.blend")
+  heldSheild = sheild.instantiate()
+  handL.add_child(heldSheild)
+
+
 
   #store animation player
-  animPlayer = $Player2/AnimationPlayer
   animPlayer.current_animation = "Idle"
   animPlayer.speed_scale = 1
   animPlayer.animation_finished.connect(_on_animation_player_animation_finished)
@@ -35,17 +47,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
   player_movement(delta)
-  print(animPlayer.assigned_animation)
-  if Input.is_action_just_pressed("mouse_lmb") and animPlayer.assigned_animation.contains("Idle") :
-    animPlayer.play("swing1")
-  elif Input.is_action_just_pressed("mouse_lmb") and animPlayer.assigned_animation.contains("swing1"):
-    animPlayer.play("swing2")
-    $lmb_wait_timer.stop()
-  elif Input.is_action_just_pressed("mouse_lmb") and animPlayer.assigned_animation.contains("swing2"):
-    animPlayer.play("swing1")
-    $lmb_wait_timer.stop()
-    pass
-
+  check_mouse_buttons()
 
 func _input(event):
   if event is InputEventMouseMotion:
@@ -69,24 +71,36 @@ func player_movement(delta):
   velocity = direction.normalized() * speed * delta
   move_and_slide()
 
-func picupweapon(weapon:Node3D):
+func check_mouse_buttons():
+  if Input.is_action_just_pressed("mouse_rmb") and animPlayer.assigned_animation.contains("Idle"):
+    animPlayer.play("block")
+  elif  Input.is_action_just_released("mouse_rmb"):
+    animPlayer.play_backwards("block")
+    animPlayer.assigned_animation = "Idle"
+  if Input.is_action_just_pressed("mouse_lmb") and animPlayer.assigned_animation.contains("Idle") :
+    animPlayer.play("swing1")
+  elif Input.is_action_just_pressed("mouse_lmb") and animPlayer.assigned_animation.contains("swing1"):
+    animPlayer.play("swing2")
+    $lmb_wait_timer.stop()
+  elif Input.is_action_just_pressed("mouse_lmb") and animPlayer.assigned_animation.contains("swing2"):
+    animPlayer.play("swing1")
+    $lmb_wait_timer.stop()
+  print(animPlayer.assigned_animation)
+
+func pic_up_weapon(weapon:Node3D):
   pass
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-  if anim_name.contains("swing1"):
-    $lmb_wait_timer.start()
-    print("anim swing1 over")
-  elif anim_name.contains("swing2"):
-    $lmb_wait_timer.start()
-    print("anim swing2 over")
-  elif anim_name.contains("Idle"):
-    print("anim Idle over")
-  else:
-    print("unknown")
-    #animPlayer.assigned_animation = "Idle"
-  pass
+  match anim_name:
+    "Idle":
+      pass
+    "block":
+      print("NOT starting timer")
+    _:
+      $lmb_wait_timer.start()
+      print("started timer")
+  return
 
 func _on_lmb_wait_timer_timeout() -> void:
   animPlayer.play("Idle")
-  print(animPlayer.assigned_animation)
   pass # Replace with function body.
