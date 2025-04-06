130e72c5f2e10534e057966a378b89739d8b4c4e
Ben Eng <whysguy52@gmail.com>
2025-04-05 21:42:07 -0500

    player updated hand position

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -7,5 +7,13 @@ func _ready() -> void:
   #temprary load sword
   sword = load("res://Assets/Items/Weapons/Sword/sword.tscn")
   heldWeapon = sword.instantiate()
-  $HandR.add_child(heldWeapon)
+  $Player2/HandR.add_child(heldWeapon)
+  pass
+
+func _physics_process(delta: float) -> void:
+  camera_update(delta)
+  pass
+
+func camera_update(delta):
+  #rotate_x(0.01)
   pass
